b1e5406188ff2d3d56709bb191e11377ef1e162e
Argha Biswas <121356992+Argha-OG@users.noreply.github.com>
2025-10-12 21:15:19 +0800

    MainLayout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router";
 
 // Import layout and pages
-import { MainLayout } from "./layout/Mainlayout";
+import { MainLayout } from "./layout/MainLayout.jsx";
 import HomePage from "./pages/homepage/HomePage";
 
 
