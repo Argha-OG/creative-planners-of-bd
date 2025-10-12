6659472661c51eea62bd236afee8fd871fda6aae
Argha Biswas <121356992+Argha-OG@users.noreply.github.com>
2025-10-13 00:46:25 +0800

    home alert

--- frontend/src/pages/homepage/HomePage.jsx
+++ frontend/src/pages/homepage/HomePage.jsx
@@ -5,6 +5,31 @@ const HomePage = () => {
   return (
     <div className="">
       <section className="lg:grid lg:h-screen lg:place-content-center overflow-hidden">
+        <div
+          role="alert"
+          className="alert alert-vertical sm:alert-horizontal mt-14 text-red-500 rounded-full"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            className="stroke-info h-6 w-6 shrink-0 text-red-500 dark:text-red-600"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+            ></path>
+          </svg>
+          <div>
+            <p className="font-bold text-xl text-red-500 dark:text-red-600!">
+              We Are Now In Malaysia
+            </p>
+          </div>
+          <button className="btn btn-sm">Explore Our services</button>
+        </div>
+
         <div className="mx-auto w-screen max-w-screen-xl px-4 py-16 sm:px-6 sm:py-24 md:grid md:grid-cols-2 md:items-center md:gap-4 lg:px-8 lg:py-32 ">
           <div className="max-w-prose text-left">
             <h1 className="text-4xl font-bold text-gray-900 sm:text-5xl ">
